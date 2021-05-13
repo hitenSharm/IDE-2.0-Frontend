e574566aaa98559811f719319a589ac0522012b6
hitenSharm <hiten.sharm1708@gmail.com>
2021-05-13 12:13:38 +0530

    made loader and some stuff

--- src/api/api.js
+++ src/api/api.js
@@ -1,38 +1,38 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseUrlAccount="http://localhost:3000/api/user/"
-const baseUrlCode="http://localhost:3000/coding/"
+const baseUrlAccount = "http://localhost:3000/api/user/";
+const baseUrlCode = "http://localhost:3000/coding/";
 
 const config = {
-    headers: {
-      "Content-type": "application/json",
-    },
-  };
+  headers: {
+    "Content-type": "application/json",
+  },
+};
 
-export const registerUser = async (formData) =>{
-  var res;  
-  const url=baseUrlAccount+'register';    
-    await axios.post(url,formData,config).then((response) =>{                
-        res=response.data;
-    })
-    return res;
-}
-
-export const loginUser = async (formData) =>{
+export const registerUser = async (formData) => {
   var res;
-  const url=baseUrlAccount+"login";
-  await axios.post(url,formData,config).then((response) =>{                
-    res=response.data;
-  })
+  const url = baseUrlAccount + "register";
+  await axios.post(url, formData, config).then((response) => {
+    res = response.data;
+  });
   return res;
-}
+};
 
-export const codeRun = async (formData)=>{
+export const loginUser = async (formData) => {
   var res;
-  const url =baseUrlCode+"coderunner"
-  await axios.post(url,formData,config).then((resp)=>{
-    res=resp.data;
+  const url = baseUrlAccount + "login";
+  await axios.post(url, formData, config).then((response) => {
+    res = response.data;
+  });
+  return res;
+};
+
+export const codeRun = async (formData) => {
+  var res;
+  const url = baseUrlCode + "coderunner";
+  await axios.post(url, formData, config).then((resp) => {
+    res = resp.data;
     console.log(resp);
-  })
+  });
   return res;
-}
+};
